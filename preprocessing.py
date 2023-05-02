88b0f1e53478032540da44f371bec21e8da82f71
p0ise <changelf@163.com>
2023-05-02 19:00:46 +0800

    对于修复预处理移除前缀的正则表达式，增加了一些常见的前缀和分隔符的更改，你可以编写如下的提交信息：
    makefile
    
    fix: 修复预处理移除前缀的正则表达式，增加常见前缀和分隔符
    
    修复了预处理移除前缀的正则表达式，以确保正确地移除常见的前缀和分隔符，使得解码器可以正确地处理各种格式的密文。同时，增加了一些常见的前缀和分隔符，以提高解码器的兼容性和可用性。
    
    这个更改解决了一个潜在的预处理问题，提高了解码器的兼容性和稳定性，使得用户可以更加方便地使用解码器。

--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,12 @@ import re
 
 
 def remove_prefixes(encoded_text):
-    encoded_text = re.sub(r'^(0x|\\x)', '', encoded_text)
+    encoded_text = re.sub(r'(0[xX]|\\[xX]|0[bB])', '', encoded_text)
     return encoded_text
 
 
 def remove_separators(encoded_text):
-    encoded_text = re.sub(r'[-:\s]', '', encoded_text)
+    encoded_text = re.sub(r'[-:;,\s]', '', encoded_text)
     return encoded_text
 
 

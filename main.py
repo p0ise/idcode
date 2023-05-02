6cf8bf1b37f8feeb48247d6f7cb13aa252dbaf6f
p0ise <changelf@163.com>
2023-05-02 19:11:00 +0800

    feat: 支持在无参数时交互式地提示用户输入密文
    增加了交互式的提示，使得用户可以在没有命令行参数的情况下输入密文。当用户运行程序时没有提供密文时，程序会提示用户输入密文，并进行后续的解码过程。
    
    这个更改提高了程序的易用性，使得用户可以更加方便地使用程序，并在不需要输入命令行参数的情况下进行解码操作。

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def read_encoded_text_from_file(file_path):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="解码器：支持命令行输入和文件输入")
-    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group = parser.add_mutually_exclusive_group()
     input_group.add_argument('-f', '--file', type=str, help="包含密文的文件路径")
     input_group.add_argument('-t', '--text', type=str, help="直接输入密文")
 
@@ -28,6 +28,13 @@ def get_encoded_text(args):
         return read_encoded_text_from_file(args.file)
     elif args.text:
         return args.text
+    else:
+        while True:
+            encoded_text = input("请输入密文：\n")
+            if encoded_text:
+                return encoded_text
+            else:
+                print("密文不能为空，请重新输入。")
 
 
 if __name__ == '__main__':

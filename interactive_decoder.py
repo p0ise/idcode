1f7b1e78741b3b81a8b374c048d33f7cff9e41ba
p0ise <changelf@163.com>
2023-05-02 19:02:21 +0800

    对于独立了状态回溯选项，修复了预处理不保存结果的 bug 的更改，你可以编写如下的提交信息：
    makefile
    
    feat: 独立状态回溯选项，修复预处理不保存结果的 bug
    
    独立了状态回溯选项，使得用户可以更加方便地控制解码过程。同时，修复了预处理不保存结果的 bug，以确保预处理可以正确地移除前缀和分隔符，并保存结果供后续处理使用。
    
    这个更改提高了解码器的可用性和稳定性，使得用户可以更加方便地使用解码器，并确保预处理可以正确地处理密文并保存结果。

--- interactive_decoder.py
+++ interactive_decoder.py
@@ -1,10 +1,18 @@
 from preprocessing import remove_prefixes, remove_separators, unify_case
 
+
 class InteractiveDecoder:
     def __init__(self, encodings, encoded_text):
         self.encodings = encodings
         self.initial_state = {'text': encoded_text, 'steps': []}
         self.states = [self.initial_state]
+
+    def go_back(self):
+        if len(self.states) > 1:
+            # Remove the current state and go back to the previous one.
+            self.states.pop()
+        else:
+            print(self.color_text("已经是初始状态，无法回退。", 91))
 
     def color_text(self, text, color_code):
         return f"\033[{color_code}m{text}\033[0m"
@@ -16,8 +24,11 @@ class InteractiveDecoder:
         choice = int(input("输入你的选择："))
         return choice
 
-    def preprocess_menu(self, current_text):
+    def preprocess_menu(self):
         while True:
+            current_state = self.states[-1]
+            current_text = current_state['text']
+
             print(self.color_text("\n选择预处理操作：", 94))
             menu_options = [
                 "去除前缀",
@@ -42,14 +53,15 @@ class InteractiveDecoder:
                 print(self.color_text("无效的输入，请重新输入。", 91))
                 continue
 
-            new_steps = self.states[-1]['steps'] + [operation]
-            new_state = {'text': new_text, 'steps': new_steps}
-            self.states.append(new_state)
+            if new_text == current_text:
+                print(self.color_text("无需进行该操作。", 93))
+            else:
+                new_steps = self.states[-1]['steps'] + [operation]
+                new_state = {'text': new_text, 'steps': new_steps}
+                self.states.append(new_state)
 
-            print(self.color_text("\n预处理后的密文：", 96))
-            print(new_text)
-
-        return current_text
+                print(self.color_text("\n预处理后的密文：", 96))
+                print(new_text)
 
     def detect_and_decode(self, current_text):
         successful_results = []
@@ -59,15 +71,20 @@ class InteractiveDecoder:
             if encoding.is_valid(current_text):
                 success, decoded_text = encoding.try_decode(current_text)
                 if success:
-                    successful_results.append((decoded_text, encoding.__name__))
+                    successful_results.append(
+                        (decoded_text, encoding.__name__))
                 else:
                     detected_encodings.append(encoding.__name__)
 
         return detected_encodings, successful_results
 
-    def decode_menu(self, current_text):
+    def decode_menu(self):
         while True:
-            detected_encodings, successful_results = self.detect_and_decode(current_text)
+            current_state = self.states[-1]
+            current_text = current_state['text']
+
+            detected_encodings, successful_results = self.detect_and_decode(
+                current_text)
 
             if not successful_results and not detected_encodings:
                 print(self.color_text("无法继续解码。", 93))
@@ -81,7 +98,8 @@ class InteractiveDecoder:
             print(self.color_text("\n成功解码的结果：", 92))
             if successful_results:
                 for i, (decoded_text, encoding_used) in enumerate(successful_results):
-                    print(f"{i + 1}. 编码算法：{encoding_used}\n解码后的文本：\n{decoded_text}\n" + "-" * 40)
+                    print(
+                        f"{i + 1}. 编码算法：{encoding_used}\n解码后的文本：\n{decoded_text}\n" + "-" * 40)
             else:
                 print("无")
 
@@ -92,10 +110,7 @@ class InteractiveDecoder:
                 break
 
             if choice == -1:
-                if len(self.states) > 1:
-                    self.states.pop()  # Remove the current state and go back to the previous one.
-                else:
-                    print(self.color_text("已经是初始状态，无法回退。", 91))
+                self.go_back()
             else:
                 chosen_result = successful_results[choice - 1]
                 new_text = chosen_result[0]
@@ -104,8 +119,6 @@ class InteractiveDecoder:
                 new_state = {'text': new_text, 'steps': new_steps}
                 self.states.append(new_state)
                 current_text = new_text
-
-        return current_text
 
     def main_menu(self):
         while True:
@@ -118,20 +131,21 @@ class InteractiveDecoder:
             menu_options = [
                 "预处理密文",
                 "检测编码并尝试解码",
+                "返回上一步",
                 "退出"
             ]
             choice = self.display_menu(menu_options)
 
             if choice == 1:
-                current_text = self.preprocess_menu(current_text)
+                self.preprocess_menu()
             elif choice == 2:
-                current_text = self.decode_menu(current_text)
+                self.decode_menu()
             elif choice == 3:
+                self.go_back()
+            elif choice == 4:
                 break
             else:
                 print(self.color_text("无效的输入，请重新输入。", 91))
-
-            current_state['text'] = current_text
 
     def run(self):
         self.main_menu()
